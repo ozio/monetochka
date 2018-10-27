2b98889f2eba2a0d35dbff6aa3b1b6f31dca050b
Nikolay Solovyov <i@mr-ozio.ru>
2018-10-28 00:52:52 +0200

    declaration files

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import { IMonetochkaFormat } from '../index';
+export declare const format: ({ value, currency, precision, k }: IMonetochkaFormat) => string;
+declare const _default: {
+    format: ({ value, currency, precision, k }: IMonetochkaFormat) => string;
+};
+export default _default;
